6616f11511573688bf0035149f339c6d5319294d
e <zhaolu@xinshiyun.com>
2018-04-10 17:51:15 +0800

    add

---
+++ routes/login.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const logger = require('../logs/log').logger;
+
+router.get('/login', function(req, res, next) {
+  // const username = req.body.username;
+  // const password = req.body.password;
+  res.json({username:'admin'});
+});
+
+module.exports = router;
