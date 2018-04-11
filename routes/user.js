b478001d77f14f4129471a45ebcdf5d4ff6743be
e <zhaolu@xinshiyun.com>
2018-04-11 17:56:05 +0800

    add

--- routes/user.js
+++ routes/user.js
@@ -3,37 +3,59 @@ const router = express.Router();
 const logger = require('../logs/log').logger;
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-router.get('/insert', function (req, res, next) {
-  let user = new User({
-    username: 'admin',
-    password: '123456'
-  })
-  user.save(function (err) {
+//用户登录
+router.get('/login', function (req, res, next) {
+  const _username = req.query.username;
+  const _password = req.query.password;
+  User.findOne({ username: _username }, function (err, doc) {
     if (err) {
       logger.error(err);
-      res.json({ code: 500, data: null, message: 'insert user failed' });
+      res.json({ code: 600, data: null, message: '查询失败' });
       return;
     }
-    return res.json({ code: 200, data: null });
+    if (doc) {
+      bcrypt.compare(_password, doc.password, function (err, flag) {
+        if (flag) {
+          return res.json({ code: 200, data: null, message: '权限验证成功' });
+        }
+        return res.json({ code: 600, data: null, message: '用户名或密码错误' });
+      });
+    }
   })
 
 });
 
-router.get('/login', function (req, res, next) {
-  const username = req.query.username;
-  const password = req.query.password;
-  User.findOne({ username: username }, function (err, doc) {
+//用户注册
+router.get('/register', function (req, res, next) {
+  const _username = req.query.username;
+  const _password = req.query.password;
+  User.findOne({ username: _username }, function (err, doc) {
     if (err) {
       logger.error(err);
-      res.json({ code: 500, data: null, message: 'query user failed.' });
+      res.json({ code: 600, data: null, message: '查询失败' });
       return;
     }
-    if (doc && doc.password === password) {
-      return res.json({ code: 200, data: null, message: 'user authentication success.' });
-    } else {
-      return res.json({ code: 500, data: null, message: 'user authentication failed.' });
+    if (doc && doc.username === _username) {
+      return res.json({ code: 601, data: null, message: '用户名已存在' });
     }
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      bcrypt.hash(_password, salt, function (err, hash) {
+        let user = new User({
+          username: _username,
+          password: hash
+        })
+        user.save(function (err, doc) {
+          if (err) {
+            logger.error(err);
+            return res.json({ code: 600, data: null, message: '创建用户失败' });
+          }
+          return res.json({ code: 200, data: null, message: '成功创建用户' });
+        })
+      });
+    });
   })
 
 });
