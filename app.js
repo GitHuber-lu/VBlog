b09cf6005acd1e9213929c7ab22132f868e96676
e <zhaolu@xinshiyun.com>
2018-04-17 14:40:19 +0800

    add

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const log4js = require('./logs/log');
+// const config = require('./config/config.js')
 const mongoose = require('./config/mongoose.js');
 const db = mongoose();
 const session = require('express-session');
@@ -23,7 +24,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -33,6 +34,7 @@ app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By", ' 3.2.1');
   res.header("Content-Type", "application/json;charset=utf-8");
+  res.header("Access-Control-Max-Age", 86400)
   next();
 });
 
@@ -51,7 +53,7 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
--- routes/article.js
+++ routes/article.js
@@ -6,9 +6,9 @@ const Article = mongoose.model('Article');
 
 //发布文章
 router.put('/publishArticle', function (req, res, next) {
-  let _title = req.body.title;
-  let _content = req.body.content;
-  let article = new Article({
+  const _title = req.body.title;
+  const _content = req.body.content;
+  const article = new Article({
     title: _title,
     content: _content
   })
@@ -22,14 +22,31 @@ router.put('/publishArticle', function (req, res, next) {
 
 });
 
-//获得文章
-router.get('/getArticleList', function (req, res, next) {
+//获得文章简介列表
+router.get('/getArticleIntroList', function (req, res, next) {
   Article.find(function (err, doc) {
     if (err) {
       logger.error(err);
       return res.json({ code: 'error', data: null, message: '查询文章列表出错' })
     }
-    return res.json({ code: 'success', data: doc, message: '获取文章列表成功' })
+    let temp = [];
+    for (let item of doc) {
+      temp.push({ id: item._id, title: item.title, content: item.content.replace(/<[^>]+>/g, '') })
+    }
+    return res.json({ code: 'success', data: temp, message: '获取文章列表成功' })
+  })
+
+});
+
+//获得文章详情
+router.get('/getArticleDetail', function (req, res, next) {
+  const id = req.params.id;
+  Article.findOne({ _id: id }, function (err, doc) {
+    if (err) {
+      logger.error(err);
+      return res.json({ code: 'error', data: null, message: '获取文章详情出错' })
+    }
+    return res.json({ code: 'success', data: doc, message: '获取文章详情成功' })
   })
 
 });
